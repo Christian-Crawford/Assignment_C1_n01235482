bd45fc43fe992a015cf1d52e02971fce17f641ba
Christian Crawford <cjcrawford216@gmail.com>
2021-11-29 19:43:21 -0500

    Add project files.

---
+++ Assignment_C1/Controllers/TeacherController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_C1.Models;
+
+namespace Assignment_C1.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        [HttpGet]
+        public ActionResult List()
+        {
+            TeacherDataController Controller = new TeacherDataController();
+            IEnumerable<Teacher> Teacher = Controller.ShowTeachers();
+            return View(Teacher);
+        }
+
+
+        [HttpGet]
+        // GET: Teacher/Show/{id}
+        [Route("Teacher/Show/{id}")]
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+            return View(NewTeacher);
+        }
+    }
+}
